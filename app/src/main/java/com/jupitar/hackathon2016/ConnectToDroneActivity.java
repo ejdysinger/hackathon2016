92d749c7167f563a9b8a9cd2844ca7a717755de0
tkoch <tkoch@email.arizona.edu>
2016-01-23 11:21:24 -0700

    adding a connect to drone button

---
+++ app/src/main/java/com/jupitar/hackathon2016/ConnectToDroneActivity.java
@@ -0,0 +1,17 @@
+package com.jupitar.hackathon2016;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class ConnectToDroneActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_connect_to_drone);
+    }
+}
